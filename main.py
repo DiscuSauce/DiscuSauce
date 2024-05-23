90fb4640bfe6e9fc3eb7424207d1bc0f6060f310
Aaron <160346138+aaron667@users.noreply.github.com>
2024-05-24 03:43:16 +0500

    Update main.py

--- main.py
+++ main.py
@@ -261,4 +261,5 @@ def admin_delete_post(post_id):
     return redirect(url_for('admin'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
